25255c1a1e3b704356cc145fdb7718772d7c4e6b
Auska <luodan0709@live.cn>
2026-01-01 13:30:16 +0800

    refactor: remove unused IPGeoInfo type definition
    - Remove IPGeoInfo struct from types.h and types.cpp
    - Remove test_types.cpp as it tested unused type
    - Update CMakeLists.txt to exclude test_types.cpp
    - Reduce code size by ~198 lines
    - Main code uses nlohmann::json directly for data transfer

--- src/types.cpp
+++ src/types.cpp
@@ -1,30 +1,9 @@
 #include "types.h"
 
+// This file is kept for future use but currently contains no implementations
+
 namespace ip_server {
 
-nlohmann::json IPGeoInfo::to_json() const {
-    nlohmann::json j;
-    j["ip"] = ip;
-    j["found"] = found;
-
-    if (!error.empty()) {
-        j["error"] = error;
-        return j;
-    }
-
-    if (found) {
-        if (!country.empty()) j["country"] = country;
-        if (!country_code.empty()) j["country_code"] = country_code;
-        if (!city.empty()) j["city"] = city;
-        if (!continent.empty()) j["continent"] = continent;
-        if (latitude != 0.0) j["latitude"] = latitude;
-        if (longitude != 0.0) j["longitude"] = longitude;
-        if (!timezone.empty()) j["timezone"] = timezone;
-        if (!as_organization.empty()) j["as_organization"] = as_organization;
-        if (as_number != 0) j["as_number"] = as_number;
-    }
-
-    return j;
-}
+// Future implementations can be added here
 
 } // namespace ip_server
--- src/types.h
+++ src/types.h
@@ -1,29 +1,10 @@
 #pragma once
 
-#include <nlohmann/json.hpp>
-#include <string>
+// This file is kept for future use but currently contains no type definitions
+// The project uses nlohmann::json directly for data transfer
 
 namespace ip_server {
 
-struct IPGeoInfo {
-    std::string ip;
-    bool found = false;
-    std::string error;
-
-    // Location info
-    std::string country;
-    std::string country_code;
-    std::string city;
-    std::string continent;
-    double latitude = 0.0;
-    double longitude = 0.0;
-    std::string timezone;
-
-    // AS info
-    std::string as_organization;
-    uint32_t as_number = 0;
-
-    nlohmann::json to_json() const;
-};
+// Future type definitions can be added here
 
 } // namespace ip_server
